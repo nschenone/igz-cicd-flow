8577ee4b27442986dcc0b49302f4e1f94da22157
Nick Schenone <nschenone16@gmail.com>
2023-09-19 00:30:18 +0000

    Added entrypoint main script with configs per workflow

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
-import os
-import yaml
+import click
 
-from src import create_and_set_project
+from src import create_and_set_project, load_config, TrainConfig, EvaluateConfig, DeployConfig
 
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+
+config = load_config()
+
+
+PIPELINE_MAPPING = {
+    "train" : TrainConfig,
+    "evaluate" : EvaluateConfig,
+    "deploy" : DeployConfig
+}
+
+SOURCE_MAPPING = {
+    "local" : config.local_source,
+    "git" : config.git_source
+}
+
+@click.command()
+@click.option(
+    "--workflow-name",
+    type=click.Choice(list(PIPELINE_MAPPING.keys())),
+    required=True,
+    help="Specify the workflow name.",
+)
+@click.option(
+    "--source",
+    type=click.Choice(list(SOURCE_MAPPING.keys())),
+    required=True,
+    help="Specify the source.",
+    default="local"
+)
+def main(
+    workflow_name: str,
+    source: str
+) -> None:
+    global config
     
-project = create_and_set_project(git_source="git://github.com/igz-us-sales/igz-liveops-demo#master")
+    print(f"Loading project {config.project_name} with source {source}")
+    project = create_and_set_project(name=config.project_name, git_source=SOURCE_MAPPING[source])
+    
+    print(f"Loading config for workflow {workflow_name}...")
+    config = load_config()
+    workflow_config = PIPELINE_MAPPING[workflow_name](**config.dict())
+    
+    print(f"Running workflow {workflow_name}...")
+    run_id = project.run(
+        name=workflow_name,
+        arguments=workflow_config.dict(),
+        dirty=True,
+        watch=True
+    )
+    
 
-run_id = project.run(name="main", arguments=config, dirty=True, watch=True)
+
+if __name__ == "__main__":
+    main()
--- src/config.py
+++ src/config.py
@@ -1,13 +1,23 @@
+from typing import Optional
 import yaml
 
-from pydantic import BaseModel
+from pydantic import BaseModel, BaseSettings
 
 
-def load_config(config_path: str = "config.yaml"):
-    with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
-    return config
-
+class Config(BaseSettings):
+    project_name: str
+    local_source: str
+    git_source: str
+    source_url: str
+    label_column: str
+    allow_validation_failure: bool
+    ohe_columns: list
+    test_size: float
+    model_name: str
+    model_path: Optional[str]
+    train_set: Optional[str]
+    test_set: Optional[str]
+    
 
 class TrainConfig(BaseModel):
     source_url: str
@@ -31,3 +41,8 @@ class DeployConfig(BaseModel):
     label_column: str
     model_name: str
     
+    
+def load_config(config_path: str = "config.yaml") -> Config:
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    return Config(**config)
